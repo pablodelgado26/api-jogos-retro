2ac6cc28d47e006da3e4d7279face8954c6ac84c
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-05-13 09:32:54 -0300

    refactor: add find user by nickname verification controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,103 +3,114 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 class AuthController {
-
-    // Listar todos os usuários
-    async getAllUsers(req, res) {
-        try {
-            const users = await UserModel.findAll();
-            res.json(users);
-        } catch (error) {
-            console.error("Erro ao listar usuários:", error);
-            res.status(500).json({ error: "Erro ao listar usuários" });
-        }
+  // Listar todos os usuários
+  async getAllUsers(req, res) {
+    try {
+      const users = await UserModel.findAll();
+      res.json(users);
+    } catch (error) {
+      console.error("Erro ao listar usuários:", error);
+      res.status(500).json({ error: "Erro ao listar usuários" });
     }
+  }
 
-    //Registrar um novo usuário
-    async register(req, res) {
-        try {
-            const { name, email, password } = req.body;
+  // Registrar novo usuário
+  async register(req, res) {
+    try {
+      const { name, nickname, email, password } = req.body;
 
-            // Validação básica
-            if (!name || !email || !password) {
-                return res.status(400).json({ error: "Os campos nome, email ou senha são obrigatórios" });
-            }
+      // Validação básica
+      if (!name || !nickname || !email || !password) {
+        return res
+          .status(400)
+          .json({ error: "Os campos nome, nickname, email e senha são obrigatórios!" });
+      }
 
-            //Verificar se o usuário ja existe 
-            const userExists = await UserModel.findByEmail(email)
+      // Verificar se o usuário já existe
+      const userEmailExists = await UserModel.findByEmail(email);
+      if (userEmailExists) {
+        return res.status(400).json({ error: "Este email já está em uso!" });
+      }
 
-            if (userExists) {
-                return res.status(400).json({ error: "Este email já está em uso!" })
-            }
+      /* const userNicknameExists = await UserModel.findByNickname(nickname);
+      if (userNicknameExists) {
+        return res.status(400).json({ error: "Este nickname já está em uso!" });
+      } */
 
-            //hash da senha
-            const hashedPassword = await bcrypt.hash(password, 10);
+      // Hash da senha
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-            //Criar o objeto do usuário
-            const data = {
-                name,
-                email,
-                password: hashedPassword,
-            };
+      // Criar objeto do usuário
+      const data = {
+        name,
+        nickname,
+        email,
+        password: hashedPassword,
+      };
 
-            //Criar usuário 
-            const user = await UserModel.create(data);
+      // Criar usuário
+      const user = await UserModel.create(data);
 
-            return res.status(201).json({
-                message: "Usuário criado com sucesso!",
-                user,
-            });
-        } catch (error) {
-            console.error("Erro ao criar novo usuario: ", error)
-            res.status(500).json({ error: "Erro ao criar novo usuário" })
-        }
+      return res.status(201).json({
+        message: "Usuário criado com sucesso!",
+        user,
+      });
+    } catch (error) {
+      console.error("Erro ao criar um novo usuário: ", error);
+      res.status(500).json({ error: "Erro ao criar um novo usuário" });
     }
+  }
 
-    async login(req, res) {
-        try {
-            const { email, password } = req.body
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
 
-            // Validação básica
-            if (!email || !password) {
-                return res.status(400).json({ error: "Os campos email e senha são obrigatórios" });
-            }
+      // Validação básica
+      if (!email || !password) {
+        return res
+          .status(400)
+          .json({ error: "Os campos email e senha são obrigatórios!" });
+      }
 
-            //Verificar se o usuário existe 
-            const userExists = await UserModel.findByEmail(email)
-            if (!userExists) {
-                return res.status(401).json({ error: "Credenciais inválidas!" })
-            }
+      // Verificar se o usuário existe
+      const userExists = await UserModel.findByEmail(email);
+      if (!userExists) {
+        return res.status(401).json({ error: "Credenciais inválidas!" });
+      }
 
-            //Verificar senha 
-            const isPasswordValid = await bcrypt.compare(password, userExists.password);
-            if (!isPasswordValid) {
-                return res.status(401).json({ error: "Credenciais inválidas!" })
-            }
+      // Verificar senha
+      const isPasswordValid = await bcrypt.compare(
+        password,
+        userExists.password
+      );
+      if (!isPasswordValid) {
+        return res.status(401).json({ error: "Credenciais inválidas!" });
+      }
 
-            //Gerar Token JWT
-            const token = jwt.sign(
-                {
-                    id: userExists.id,
-                    name: userExists.name,
-                    email: userExists.email,
-                },
-                process.env.JWT_SECRET,
-                {
-                    expiresIn: "24h",
-                }
-            );
-
-            return res.json({
-                message: "Login realizado com sucesso!",
-                token,
-                userExists,
-            });
-        } catch (error) {
-            console.error("Erro ao fazer login: ", error)
-            res.status(500).json({message: "Erro ao fazer login!"})
+      // Gerar Token JWT
+      const token = jwt.sign(
+        {
+          id: userExists.id,
+          name: userExists.name,
+          nickname: userExists.nickname,
+          email: userExists.email,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: "24h",
         }
-    }
+      );
 
+      return res.json({
+        message: "Login realizado com sucesso!",
+        token,
+        userExists,
+      });
+    } catch (error) {
+      console.error("Erro ao fazer login: ", error);
+      res.status(500).json({ error: "Erro ao fazer login!" });
+    }
+  }
 }
 
 export default new AuthController();
