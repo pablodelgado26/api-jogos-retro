a853ed2d3262d2aa543677dfbb6cb800ca5fcd7c
Pablo Delgado <pablo.j.abreu@aluno.senai.br>
2025-05-13 08:20:51 -0300

    refactor: ajustando projeto

--- prisma/seed/seed.js
+++ prisma/seed/seed.js
@@ -472,11 +472,11 @@ async function main() {
   );
 }
 
-main()
+/* main()
   .catch((e) => {
     console.error(e);
     process.exit(1);
   })
   .finally(async () => {
     await prisma.$disconnect();
-  });
+  }); */
